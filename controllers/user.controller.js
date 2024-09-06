68dfbd16e966c87fce666deb1d3b0406c4021c8f
ThanhLong1601 <72308447+ThanhLong1601@users.noreply.github.com>
2024-09-06 11:57:17 +0700

    Add router and controller getUserDetails, changePassword, updateUserInfo

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,8 +2,8 @@ const User = require("../models/user.model.js");
 const bcrypt = require("bcrypt");
 
 const createUser = async (req, res) => {
+  const { name, email, password, gender, birthdate } = req.body;
   try {
-    const { name, email, password, gender, birthdate } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({
@@ -47,7 +47,63 @@ const loginUser = async (req, res) => {
   }
 };
 
+const getUserDetails = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const changePassword = async (req, res) => {
+  const { oldPassword, newPassword, confirmPassword } = req.body;
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(oldPassword, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Old password is incorrect" });
+
+    if (newPassword !== confirmPassword)
+      return res
+        .status(400)
+        .json({ message: "New password and confirmation do not match" });
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+    await user.save();
+
+    res.json({ message: "Password changed successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateUserInfo = async (req, res) => {
+  const { name, gender, birthdate } = req.body;
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    if (name) user.name = name;
+    if (gender) user.gender = gender;
+    if (birthdate) user.birthdate = birthdate;
+
+    await user.save();
+    res.json({ message: "User info updated successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   createUser,
   loginUser,
+  getUserDetails,
+  changePassword,
+  updateUserInfo,
 };
--- routes/user.router.js
+++ routes/user.router.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const User = require("../models/user.model.js");
 const router = express.Router();
-const { createUser, loginUser } = require("../controllers/user.controller.js");
+const {
+  createUser,
+  loginUser,
+  getUserDetails,
+  changePassword,
+  updateUserInfo,
+} = require("../controllers/user.controller.js");
 
 // Sign up
 router.post("/", createUser);
@@ -9,13 +15,13 @@ router.post("/", createUser);
 // Login
 router.post("/login", loginUser);
 
-// // Get User Details
-// router.get("/:id", getUserDetails);
+// Get User Details
+router.get("/:id", getUserDetails);
 
-// // Change Password
-// router.put("/:id/change-password", changePassword);
+// Change Password
+router.put("/:id/change-password", changePassword);
 
-// // Update User Info
-// router.put("/:id/update", updateUserInfo);
+// Update User Info
+router.put("/:id/update", updateUserInfo);
 
 module.exports = router;
